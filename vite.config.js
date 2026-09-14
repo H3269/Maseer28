b13e07463b81e5534722a1b939e2cfa5bbbc0fb1
arashfar <saeedarashfar1371@gmail.com>
2026-09-14 23:52:19 +0330

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/Maseer28/',
   plugins: [react()],
-  build: { target: 'es2020', sourcemap: false },
+  build: {
+    target: 'es2020',
+    sourcemap: false,
+  },
 });
